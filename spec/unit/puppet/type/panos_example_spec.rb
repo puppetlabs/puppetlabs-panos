60ed2c92c606d495534c231978e2677839eedfe4
David Schmitt <david.schmitt@puppet.com>
2018-05-30 20:26:12 +0100

    Remove panos_example type

--- spec/unit/puppet/type/panos_example_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'puppet/type/panos_example'
-
-RSpec.describe 'the panos_example type' do
-  it 'loads' do
-    expect(Puppet::Type.type(:panos_example)).not_to be_nil
-  end
-end
