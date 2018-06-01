ca2195d2aee4c8278cb24f90fba89aa2c5ee8157
David Schmitt <david.schmitt@puppet.com>
2018-06-01 13:08:26 +0100

    Improve error handling of API responses

--- lib/puppet/util/network_device/panos/device.rb
+++ lib/puppet/util/network_device/panos/device.rb
@@ -18,6 +18,7 @@ module Puppet::Util::NetworkDevice::Panos
     end
 
     def get_config(xpath)
+      Puppet.debug("Retrieving #{xpath}")
       # https://<firewall>/api/?key=apikey&type=config&action=get&xpath=<path-to-config-node>
       uri = URI::HTTP.build(path: '/api/')
       params = { type: 'config', action: 'get', key: apikey, xpath: xpath }
@@ -28,10 +29,12 @@ module Puppet::Util::NetworkDevice::Panos
         raise "Error: #{res}: #{res.message}"
       end
       doc = REXML::Document.new(res.body)
+      handle_response_errors(doc)
       doc
     end
 
     def set_config(xpath, document)
+      Puppet.debug("Writing to #{xpath}")
       # https://<firewall>/api/?key=apikey&type=config&action=set&xpath=xpath-value&element=element-value
       uri = URI::HTTP.build(path: '/api/')
       params = { type: 'config', action: 'set', key: apikey, xpath: xpath, element: document.to_s }
@@ -42,11 +45,18 @@ module Puppet::Util::NetworkDevice::Panos
         raise "Error: #{res}: #{res.message}"
       end
       doc = REXML::Document.new(res.body)
+      handle_response_errors(doc)
       doc
+    end
+
+    def edit_config(xpath, document)
+      # https://<firewall>/api/?key=apikey&type=config&action=edit&xpath=xpath-value&element=element-value
+      raise "not implemented"
     end
 
     def delete_config(xpath)
       # https://<firewall>/api/?key=apikey&type=config&action=delete&xpath=xpath-value
+      raise "not implemented"
     end
 
     private
@@ -66,7 +76,7 @@ module Puppet::Util::NetworkDevice::Panos
 
     def apikey
       @key ||= if config.key? 'apikey'
-                 config['key']
+                 config['apikey']
                else
                  get_apikey(config['user'], config['password'])
                end
@@ -82,7 +92,61 @@ module Puppet::Util::NetworkDevice::Panos
         raise "Error: #{res}: #{res.message}"
       end
       doc = REXML::Document.new(res.body)
+      handle_response_errors(doc)
       doc.elements['/response/result/key'].text
+    end
+
+    def message_from_code(code)
+      case code
+      when "1"
+        "Unkown command: The specific config or operational command is not recognized."
+      when "2", "3", "4", "5", "11", "21"
+        "Internal error: Check with Palo Alto's technical support."
+      when "6"
+        "Bad XPath: The xpath specified in one or more attributes of the command is invalid."
+      when "7"
+        "Object not present: Object specified by the xpath is not present. For example, entry[@name='value'] where no object with name 'value' is present."
+      when "8"
+        "Object not unique: For commands that operate on a single object, the specified object is not unique."
+      when "10"
+        "Reference count not zero: Object cannot be deleted as there are other objects that refer to it. For example, address object still in use in policy."
+      when "12"
+        "Invalid object: Xpath or element values provided are not complete."
+      when "14"
+        "Operation not possible: Operation is allowed but not possible in this case. For example, moving a rule up one position when it is already at the top."
+      when "15"
+        "Operation denied: Operation is allowed. For example, Admin not allowed to delete own account, Running a command that is not allowed on a passive device."
+      when "16"
+        "Unauthorized: The API role does not have access rights to run this query."
+      when "17"
+        "Invalid command: Invalid command or parameters."
+      when "18"
+        "Malformed command: The XML is malformed."
+      when "19", "20"
+        "Success: Command completed successfully."
+      when "22"
+        "Session timed out: The session for this query timed out."
+      else
+        "Unknown error code %{code}" % {code:code}
+      end
+    end
+
+    def handle_response_errors(doc)
+      status = doc.elements['/response'].attributes['status']
+      code = doc.elements['/response'].attributes['code']
+      error_message = ('Received "%{status}" with code %{code}: %{message}' % {
+        status: status,
+        code: code,
+        message: message_from_code(code)
+      })
+      # require 'pry';binding.pry
+      if status == "success"
+        Puppet.debug(error_message)
+      else
+        error_message << "\n"
+        doc.write(error_message, 2)
+        raise Puppet::ResourceError, error_message
+      end
     end
   end
 end
