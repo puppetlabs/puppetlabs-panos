3f94af5224b658aa3e35f3910ea047f6dc730edd
Thomas-Franklin <thomas.franklin@puppet.com>
2018-09-13 16:38:36 +0100

    (maint) defaulting virtual routers entries if nil
    The virtual routers `administrative distances` are required attributes but defaults do not show in PAN-OS xml, which means on a get an incompatible type warning will occur.

--- lib/puppet/provider/panos_virtual_router/panos_virtual_router.rb
+++ lib/puppet/provider/panos_virtual_router/panos_virtual_router.rb
@@ -4,6 +4,19 @@ require 'builder'
 
 # Implementation for the panos_virtual_router type using the Resource API.
 class Puppet::Provider::PanosVirtualRouter::PanosVirtualRouter < Puppet::Provider::PanosProvider
+  def munge(entry)
+    entry[:ad_static] = '10' if entry[:ad_static].nil?
+    entry[:ad_static_ipv6] = '10' if entry[:ad_static_ipv6].nil?
+    entry[:ad_ospf_int] = '30' if entry[:ad_ospf_int].nil?
+    entry[:ad_ospf_ext] = '110' if entry[:ad_ospf_ext].nil?
+    entry[:ad_ospfv3_int] = '30' if entry[:ad_ospfv3_int].nil?
+    entry[:ad_ospfv3_ext] = '110' if entry[:ad_ospfv3_ext].nil?
+    entry[:ad_ibgp] = '200' if entry[:ad_ibgp].nil?
+    entry[:ad_ebgp] = '20' if entry[:ad_ebgp].nil?
+    entry[:ad_rip] = '120' if entry[:ad_rip].nil?
+    entry
+  end
+
   def xml_from_should(name, should)
     builder = Builder::XmlMarkup.new
     builder.entry('name' => name) do
