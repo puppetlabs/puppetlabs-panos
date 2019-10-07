2862ed912fa752da63d621f5105d25a5afebd049
Dave Armstrong <david.armstrong@puppet.com>
2019-10-07 16:42:33 +0100

    (maint) fix rubocop errors

--- lib/puppet/provider/panos_application_group/panos_application_group.rb
+++ lib/puppet/provider/panos_application_group/panos_application_group.rb
@@ -3,9 +3,7 @@ require_relative '../panos_provider'
 # Implementation for the panos_application_group type using the Resource API.
 class Puppet::Provider::PanosApplicationGroup::PanosApplicationGroup < Puppet::Provider::PanosProvider
   def validate_should(should)
-    if should[:members].nil?
-      raise Puppet::ResourceError, 'Application group should contain `members`'
-    end
+    raise Puppet::ResourceError, 'Application group should contain `members`' unless should[:members]
   end
 
   def xml_from_should(name, should)
